d40a9111421cd9cda54635cac31f30413860bb98
n4aaat <barrerasalgadonatalyitzel@gmail.com>
2025-05-28 14:28:45 -0600

    hola

---
+++ auxilia_app/lib/routes/app_routes.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// Importa tus páginas
+import '../pages/login_page.dart';
+import '../pages/register_page.dart';
+import '../pages/dashboard_page.dart';
+import '../pages/emergencia_page.dart';
+import '../pages/resultado_page.dart';
+import '../pages/decision_page.dart';
+
+class AppRoutes {
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String dashboard = '/dashboard';
+  static const String emergencia = '/emergencia';
+  static const String resultado = '/resultado';
+  static const String decision = '/decision';
+
+  static Map<String, WidgetBuilder> routes = {
+    login: (context) => const LoginPage(),
+    //register: (context) => const RegisterPage(),
+    //dashboard: (context) => const DashboardPage(),
+    //emergencia: (context) => const EmergenciaPage(),
+    //resultado: (context) => const ResultadoPage(),
+    //decision: (context) => const DecisionPage(),
+  };
+}
